213a27dd67de9da28934703b114f90b25e789979
Adrian Irving-Beer <wisq@wisq.net>
2018-08-17 17:28:25 -0400

    New project from "mix new ex_lgtv".

---
+++ lib/ex_lgtv.ex
@@ -0,0 +1,18 @@
+defmodule ExLgtv do
+  @moduledoc """
+  Documentation for ExLgtv.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExLgtv.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_lgtv_test.exs
@@ -0,0 +1,8 @@
+defmodule ExLgtvTest do
+  use ExUnit.Case
+  doctest ExLgtv
+
+  test "greets the world" do
+    assert ExLgtv.hello() == :world
+  end
+end
